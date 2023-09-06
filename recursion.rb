8d968f72f7fa6975db049cff48329605f30e316b
“Eltion <ebehrami01@gmail.com>
2023-09-06 13:52:28 -0400

    exp1 and exp2

--- recursion.rb
+++ recursion.rb
@@ -5,8 +5,49 @@ def range_rec(start_num, end_num)
     new_arr + range_rec(start_num + 1, end_num)
 end
 
+# p range_rec(1,5)
+
 def range_itr(start_num, end_num)
+    new_array = []
+    (start_num..end_num).each do |num|
+        new_array << num
+    end 
+    new_array
 end
+
+# p range_itr(1, 5)
+
+def exp1(b, n)
+    return 1 if n == 0
+    exp1(b, n -1) * b
+end 
+
+# p exp1(2, 4)
+
+def exp2(b, n)
+    return 1 if n == 0 
+    return b if n == 1
+
+    # last_call = exp2(b, n - 1)
+
+    if n.odd?
+        b * (exp(b, (n - 1) / 2) ** 2)
+    else 
+        exp2(b, n / 2) ** 2
+    end 
+
+    # last_call_even = exp2(b, n / 2)
+    # last_call_odd = exp2(b, (n - 1) / 2)
+
+
+end 
+    
+p exp2(2, 3)
+
+
+
+
+
+
     
 
-p range_rec(1,5)
